6788ae4194101a77e6808f40f6202e5f69a92645
Takano Yasuhiro <takano.yasuhiro@ekilsid.com>
2019-03-07 15:04:39 +0700

    first commit

---
+++ app/components/top.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Top extends React.Component {
+  render() {
+    console.log('top');
+    return (
+      <div>
+        <h1>Electron boilerplate</h1>
+      </div>
+    );
+  }
+}
